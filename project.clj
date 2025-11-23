d82f223bcd8c203fce83d9a5ed1b206b1a5bf832
Kassuelo <kassuelo.mo@hotmail.com>
2025-11-23 13:46:55 -0300

    First Commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject url-shortener "0.1.0-SNAPSHOT"
+  :description "Encurtador de URL simples em memória"
+  :url "https://github.com/kassuelo/url-shortener"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]
+		[ring/ring-core "1.12.1"]
+		[ring/ring-jetty-adapter "1.12.1"]
+		[compojure "1.7.0"]
+		[hiccup "1.0.5"]]
+  :repl-options {:init-ns url-shortener.core}
+  :main url-shortener.core)
